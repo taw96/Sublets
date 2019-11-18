ec608bcbe868b1beae01fee2d8133d39b14d995a
Tomer <tomeramit96@gmail.com>
2019-11-18 03:04:41 +0200

    initial

--- server.js
+++ server.js
@@ -40,10 +40,14 @@ app.use('/sublets', subletsRouter);
 // const usersRouter = require('./routes/api/users');
 
 // app.use('/users', usersRouter)
+
 // Serve static assets if in production
+
 if(process.env.NODE_ENV=== 'production'){
+
   //set static folder
-  app.use(express.static('client/build'))
+
+  app.use(express.static('client/build'));
 
   app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
