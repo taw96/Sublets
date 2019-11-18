1e6349d46b2ffae7d8b085e0b9213a0dc115998e
Tomer <tomeramit96@gmail.com>
2019-11-18 19:13:05 +0200

    fin

--- server.js
+++ server.js
@@ -43,7 +43,14 @@ app.use('/sublets', subletsRouter);
 // app.use('/users', usersRouter)
 
 // Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
